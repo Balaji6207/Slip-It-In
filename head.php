456c0358da1ae7b96f240f36d271763199679f73
Balaji <mbalaji06.23@ichat.sp.edu.sg>
2025-10-19 20:32:39 +0800

    test

--- head.php
+++
@@ -1,5 +0,0 @@
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto Condensed">
-<link rel="stylesheet" href="background.css">
-<link rel="icon" href="img/favicon.jpg">
--- tiles.php
+++
@@ -1,4 +0,0 @@
-<div class="tiles">
-    <img class="mobile" src="img/tiles1.png">
-    <img class="desktop" src="img/tiles2.png">
-</div>
